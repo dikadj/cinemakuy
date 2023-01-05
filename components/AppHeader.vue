3aaea1c9a1ccf6061bb47096dbe1b7951aeb8c28
Primadika Dwijantara <primadika.dwijantara@gmail.com>
2023-01-05 23:41:06 +0700

    fixes search submit

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -7,9 +7,9 @@
               <nuxt-link class="navbar-brand mx-2" to="/"><img src="../assets/img/logo.png" alt="CINEMAKUY"></nuxt-link>
             </div>
             <div class="col-6 col-lg-5">
-              <form class="" role="search" :action="'/s/' + searchQuery" method="post">
-                <input id="search_bar" class="form-control px-4" type="search" name="search" placeholder="Search Title / Theatre Here" aria-label="Search" :value="searchQuery" @input="inputSearch">
-                <button class="btn btn-outline-success" type="submit" hidden>Search</button>
+              <form role="search" action="" method="">
+                <input id="search_bar" class="form-control px-4" type="search" name="search" placeholder="Search Title / Theatre Here" aria-label="Search" v-model="searchQuery">
+                <button class="btn btn-outline-success" type="submit" hidden @click.stop.prevent="goSearch()">Search</button>
               </form>
             </div>
             <div class="d-none d-lg-flex justify-content-end align-items-center col-lg-5">
@@ -31,10 +31,8 @@
       }
     },
     methods: {
-      inputSearch(e) {
-        this.searchQuery = e.target.value
-        // this.$router.replace({ path: this.searchQuery })
-        // this.$router.push({ path: this.searchQuery })
+      goSearch() {
+        this.$router.push({ path: "/s/" + this.searchQuery })
       }
     },
     created() {
